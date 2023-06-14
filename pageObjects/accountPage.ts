b43f225c4e5f6ca0c3a5e8cee3fa68bdc8280722
Swathika <swathikavisagn@swathikas-air.home>
2023-06-14 23:09:38 +0100

    add or cancel subscription

---
+++ pageObjects/accountPage.ts
@@ -0,0 +1,55 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class accountPage {
+  readonly page: Page;
+  readonly newsletterHeading: Locator;
+  readonly subscription: Locator;
+  readonly yesRadio: Locator;
+  readonly noRadio: Locator;
+  readonly continueBtn: Locator;
+  readonly successAlert: Locator;
+  readonly accountBreadcrumb: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.newsletterHeading = page.getByRole("heading", { name: "Newsletter" });
+    this.subscription = page.getByRole("link", {
+      name: "Subscribe / unsubscribe to newsletter",
+    });
+    this.yesRadio = page.getByLabel("Yes");
+    this.noRadio = page.getByLabel("No");
+    this.continueBtn = page.locator('input[type="submit"]');
+    this.successAlert = page.locator(".alert.alert-success.alert-dismissible");
+    this.accountBreadcrumb = page.locator(".breadcrumb li", {
+      hasText: "Account",
+    });
+  }
+
+  async changeSubscription() {
+    if (await this.yesRadio.isChecked()) {
+      await this.noRadio.check();
+      await this.continueBtn.click();
+      await expect(this.successAlert).toBeVisible();
+
+      //assert if no is checked
+      await this.accountBreadcrumb.click();
+      await this.page.waitForLoadState("domcontentloaded");
+
+      this.subscription.click();
+      await this.page.waitForLoadState("domcontentloaded");
+      expect(await this.noRadio.isChecked()).toBeTruthy();
+    } else {
+      await this.yesRadio.check();
+      await this.continueBtn.click();
+      await expect(this.successAlert).toBeVisible();
+
+      //assert if Yes is checked
+      await this.accountBreadcrumb.click();
+      await this.page.waitForLoadState("domcontentloaded");
+
+      await this.subscription.click();
+      await this.page.waitForLoadState("domcontentloaded");
+      expect(await this.yesRadio.isChecked()).toBeTruthy();
+    }
+  }
+}
---
+++ tests/newsletter_subscription.spec.ts
@@ -0,0 +1,46 @@
+import { expect } from "@playwright/test";
+import { fixture } from "../utils/testbase";
+import { loginPage } from "../pageObjects/loginPage";
+import { homePage } from "../pageObjects/homePage";
+import { accountPage } from "../pageObjects/accountPage";
+
+fixture.beforeEach(async ({ page, data }) => {
+  const login = new loginPage(page);
+  const home = new homePage(page);
+  //go to opencart
+  await page.goto("");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Your Store/);
+
+  // Expects the URL to contain opencart
+  await expect(page).toHaveURL(/opencart/);
+
+  //access account menu
+  await login.goToLogin();
+
+  //login
+  await login.login(data.email, data.password);
+});
+
+fixture("Cancel or Add newsletter subscription", async ({ page, data }) => {
+  const account = new accountPage(page);
+  //wait for page to load
+  await page.waitForLoadState("networkidle");
+
+  //assert account page url
+  await expect(page).toHaveURL(/account/);
+
+  //click newsletter menu
+  expect(await account.newsletterHeading.isHidden()).toBeFalsy();
+
+  await account.subscription.click();
+
+  //assert newsletter page
+  await page.waitForURL(/newsletter/);
+  await expect(page).toHaveURL(/newsletter/);
+
+  //change the subscription option
+
+  await account.changeSubscription();
+});
